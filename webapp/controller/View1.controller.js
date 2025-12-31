07daf9ce1a3025a72e438fe88fb9dc92d70b130e
Gunalan Meyyappan <gunalan2170@gmail.com>
2025-12-31 08:56:01 +0000

    first commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,80 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("sample.project1.controller.View1", {
+        onInit() {
+
+            let oData = {
+                "recipient": {
+                    "name": "guna"
+                }
+            }
+
+            // let oModel = new JSONModel(oData);
+            // oModel.setDefaultBindingMode(BindingMode.OneWay);
+
+            // this.getView().setModel(oModel);
+
+            // alert("init")
+
+        },
+        // onBeforeRendering() {
+        //    alert("before")
+        // },
+        // onAfterRendering() {
+        //         console.log("ghdc")
+        // },
+        // onExit() {
+        //         console.log("ghdc")
+        // },
+
+        // onPress: function () {
+        //     const oBundle = this.getView().getModel("i18n").getResourceBundle();
+        //     const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+        //     // const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        //     const sMsg = oBundle.getText("helloMsg");
+
+        //     // show message
+        //     MessageToast.show(`${sMsg} ${sRecipient}`);
+        //     //  MessageToast.show(sMsg);
+
+
+        // }
+
+        // onPress: function () {
+        //     const oModel = this.getView().getModel("manifestModel");
+        //     const aData = this.getView().getModel("manifestModel").getData().AddressCollection;
+
+        //     const oData = {
+        //         "Name": oModel.getProperty("/SupplierName"),
+        //         "StreetNo": oModel.getProperty("/HouseNumber"),
+        //         "Code": oModel.getProperty("/City")
+        //     }
+        //     // aData.push({
+
+        //     //     "Name": "aed",
+        //     //     "StreetNo": "1/123",
+        //     //     "Code": "MM01"
+
+        //     // });
+        //     aData.push(oData);
+        //     oModel.refresh(true);
+
+
+        // },
+
+        // onCheckBoxSelect: function (oEvent) {
+        //     let bFixedLayout = oEvent.getParameter("selected");
+        //     let oTable = oEvent.getSource().getParent().getParent();
+        //     let oTable1 = this.getView().byId("addresstable");
+        //     oTable1.setFixedLayout(bFixedLayout);
+        // },
+
+
+
+
+    });
+});
